2e01817f041918a7be156f42014bab539b01bd47
Michael Bombini <bombini.michael@gmail.com>
2022-07-01 12:50:49 +0200

    Completed TheHeader

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <div id="app">
+    <TheHeader/>
+
   </div>
 </template>
 
 <script>
+import TheHeader from './components/TheHeader.vue';
 
 export default {
   name: 'App',
   components: {
-  }
+    TheHeader
+}
 }
 </script>
 
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,52 @@
+<template>
+  <header class="py-3">
+    <div class="container d-flex justify-content-between align-items-center">
+      <div>
+        <i class="fa-solid fa-clock px-1"></i>
+        Open Hours: Mon - Sat - 9:00 - 18:00
+      </div>
+      <ul class="d-flex justify-content-between">
+        <li>
+          <a href="#"><i class="fa-solid fa-phone"></i> +1 (305) 1234-5678</a>
+        </li>
+         <li>
+          <a href="#"><i class="fa-solid fa-envelope px-2"></i>hello@example.com</a>
+        </li>
+         <li>
+          <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+        </li>
+         <li>
+          <a href="#"><i class="fa-brands fa-twitter"></i></a>
+        </li>
+         <li>
+          <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+        </li>
+  
+      </ul>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/styles/variables";
+@import "../assets/styles/general";
+
+/* HEADER */
+
+header {
+  background-color: $dark-muted;
+  font-size: $text-small;
+}
+div,
+ul{
+  color: $gray-nurse;
+}
+a {color: $gray-nurse; display: block; padding: 0 1rem;}
+
+
+
+</style>
