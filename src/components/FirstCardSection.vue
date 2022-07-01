ab0c83d999a9d093960121328260ed46bba145be
Michael Bombini <bombini.michael@gmail.com>
2022-07-01 20:11:35 +0200

    Completed Component card

---
+++ src/components/FirstCardSection.vue
@@ -0,0 +1,59 @@
+<template>
+    <section>
+      <div class="container">
+        <div class="row">
+            <div class="col-4" v-for="(card,i) in sectionTreCard" :key="card + i">
+                <div class="card-section">
+                    <i :class="card.icon"></i>
+                    <h2>{{card.title}}</h2>
+                    <p>{{card.description}}</p>
+                </div>
+            </div>
+        </div>
+      </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: "FirstCardSection",
+        props : {
+            sectionTreCard : Array,
+            
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/styles/variables";
+@import "../assets/styles/general";
+section {
+    padding: $section-spacer 0;
+    background-color: $gray-nurse;
+
+ .card-section {
+    background: $white;
+    max-width: 400px;
+    
+    i {
+        background-color: #DAECED;
+        width: 60px;
+        height: 60px;
+        border-radius: 50%;
+        text-align: center;
+        line-height: 60px;
+        color: $blue-lagoon;
+    }
+
+    h2 {
+        color: $dark-muted;
+        font-weight: 700;
+    }
+
+    p {
+        color: $slate-gray;
+    }
+    
+    }   
+}
+</style>
--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -1,26 +1,58 @@
 <template>
-    <main>
-       <FirstCardSection/>
-       <TheCompany/>
-       <BusinessArea/>
-       <MonthlyPlan/>
-       <EditorialContent/>
-       <NewsLetter/>
-       <ContactForm/>
-    </main>
+  <main>
+    <FirstCardSection :sectionTreCard="sectionTreCard" />
+    <TheCompany />
+    <BusinessArea />
+    <MonthlyPlan />
+    <EditorialContent />
+    <NewsLetter />
+    <ContactForm />
+  </main>
 </template>
 
 <script>
-import FirstCardSection from './FirstCardSection.vue';
-import TheCompany from './TheCompany.vue';
-import BusinessArea from './BusinessArea.vue';
-import MonthlyPlan from './MonthlyPlan.vue';
-import EditorialContent from './EditorialContent.vue';
-import NewsLetter from './NewsLetter.vue';
-import ContactForm from './ContactForm.vue';
-    export default {
-    components: { FirstCardSection, TheCompany, BusinessArea, MonthlyPlan, EditorialContent, NewsLetter, ContactForm }
-}
+import FirstCardSection from "./FirstCardSection.vue";
+import TheCompany from "./TheCompany.vue";
+import BusinessArea from "./BusinessArea.vue";
+import MonthlyPlan from "./MonthlyPlan.vue";
+import EditorialContent from "./EditorialContent.vue";
+import NewsLetter from "./NewsLetter.vue";
+import ContactForm from "./ContactForm.vue";
+export default {
+  components: {
+    FirstCardSection,
+    TheCompany,
+    BusinessArea,
+    MonthlyPlan,
+    EditorialContent,
+    NewsLetter,
+    ContactForm,
+  },
+  data() {
+    return {
+      sectionTreCard: [
+        {
+          icon: "fa-solid fa-user-group",
+          title: "Human Capital",
+          description:
+            "Humanizing business: Harness the power of technology to improve the way people work.",
+        },
+        {
+          icon: "fa-solid fa-chart-pie",
+          title: "Core Business",
+          description:
+            "It takes innovative approaches to transform, modernize, and run existing platforms.",
+        },
+           {
+          icon: "fa-solid fa-gauge-high",
+          title: "Performance",
+          description:
+            "Achieving maximum impact and value from investments in finance and supply chain.",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
